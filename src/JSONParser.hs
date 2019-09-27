a65b05c4e59fe4933d51a6279d97245858c774b1
Hive7 <oliver@cole2.com>
2019-09-27 13:21:21 +0100

    Cleaned up code and added ContraintKinds to avoid repetition

--- src/JSONParser.hs
+++ src/JSONParser.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ConstraintKinds #-}
+
 module JSONParser (parseJSON) where
 
 import           JSONTypes
@@ -7,9 +9,13 @@ import           Control.Applicative
 import           Control.Monad
 import           Data.Char
 
-type SParser m a = StateT String m a
+type SParser = StateT String
 
-ch :: MonadFail m => (Char -> Bool) -> SParser m Char
+type FCst m = MonadFail m
+
+type FPCst m = (MonadPlus m, FCst m)
+
+ch :: FCst m => (Char -> Bool) -> SParser m Char
 ch f = do
   (x:xs) <- get
   put xs
@@ -17,62 +23,61 @@ ch f = do
     then return x
     else F.fail "Parse Error"
 
-space :: (MonadPlus m, MonadFail m) => SParser m Char
+space :: FCst m => SParser m Char
 space = ch (`elem` " \n\r\t")
 
-token :: (MonadPlus m, MonadFail m) => SParser m a -> SParser m a
+token :: FPCst m => SParser m a -> SParser m a
 token = (*>) $ many space
 
-tokenCh :: (MonadPlus m, MonadFail m) => Char -> SParser m Char
+tokenCh :: FPCst m => Char -> SParser m Char
 tokenCh x = token $ ch (== x)
 
-matchStr :: MonadFail m => String -> SParser m Char
+matchStr :: FCst m => String -> SParser m Char
 matchStr [] = ch $ const False
 matchStr [x] = ch (== x)
 matchStr (x:xs) = ch (== x) *> matchStr xs
 
-matchConst :: MonadFail m => a -> String -> SParser m a
+matchConst :: FCst m => a -> String -> SParser m a
 matchConst x y = x <$ matchStr y
 
-parseStr :: (MonadPlus m, MonadFail m)
-         => (SParser m Char -> SParser m String)
-         -> SParser m String
+parseStr :: FPCst m => (SParser m Char -> SParser m String) -> SParser m String
 parseStr x = token
   $ ch (== '\"') *> x (matchStr "\\\"" <|> ch (/= '\"')) <* ch (== '\"')
 
-parseString :: (MonadPlus m, MonadFail m) => SParser m JSON
+parseString :: FPCst m => SParser m JSON
 parseString = JString <$> parseStr many
 
-parseNumber :: (MonadPlus m, MonadFail m) => SParser m JSON
+parseNumber :: FPCst m => SParser m JSON
 parseNumber = JNumber . fromIntegral . read <$> some (ch isDigit)
 
-parseTrue :: MonadFail m => SParser m JSON
+parseTrue :: FCst m => SParser m JSON
 parseTrue = JBool <$> matchConst True "true"
 
-parseFalse :: MonadFail m => SParser m JSON
+parseFalse :: FCst m => SParser m JSON
 parseFalse = JBool <$> matchConst False "false"
 
-parseBool :: (MonadPlus m, MonadFail m) => SParser m JSON
+parseBool :: FPCst m => SParser m JSON
 parseBool = parseTrue <|> parseFalse
 
-parseNull :: MonadFail m => SParser m JSON
+parseNull :: FCst m => SParser m JSON
 parseNull = matchConst JNull "null"
 
-parseArr
-  :: (MonadPlus m, MonadFail m) => Char -> Char -> SParser m a -> SParser m [a]
-parseArr c c' x = (ch (== c) *> many (x <* tokenCh ',') <* tokenCh c')
-  <|> (ch (== c) *> ((:) <$> x <*> many (tokenCh ',' *> x)) <* tokenCh c')
+parseArr :: FPCst m => Char -> Char -> SParser m a -> SParser m [a]
+parseArr c c' x = ch (== c) *> sepBy x (tokenCh ',') <* tokenCh c'
 
-parseArray :: (MonadPlus m, MonadFail m) => SParser m JSON
+sepBy :: FPCst m => SParser m a -> SParser m b -> SParser m [a]
+sepBy p sep = (((:) <$> p <*> many (sep *> p)) <|> pure []) <* many sep
+
+parseArray :: FPCst m => SParser m JSON
 parseArray = JArray <$> parseArr '[' ']' parseJSONData
 
-parsePair :: (MonadPlus m, MonadFail m) => SParser m (String, JSON)
+parsePair :: FPCst m => SParser m (String, JSON)
 parsePair = (,) <$> parseStr some <* tokenCh ':' <*> parseJSONData
 
-parseObject :: (MonadPlus m, MonadFail m) => SParser m JSON
+parseObject :: FPCst m => SParser m JSON
 parseObject = JObject <$> parseArr '{' '}' parsePair
 
-parseJSONData :: (MonadPlus m, MonadFail m) => SParser m JSON
+parseJSONData :: FPCst m => SParser m JSON
 parseJSONData = token
   $ parseObject
   <|> parseArray
@@ -81,5 +86,5 @@ parseJSONData = token
   <|> parseBool
   <|> parseNull
 
-parseJSON :: (MonadPlus m, MonadFail m) => String -> m JSON
+parseJSON :: FPCst m => String -> m JSON
 parseJSON = evalStateT parseJSONData
